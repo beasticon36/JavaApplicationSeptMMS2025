b0e65e7af997ed518fd517073d2abb946992d635
beasticon36 <bozyboy360@gmail.com>
2026-05-06 20:03:14 +0100

    Hello Program

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, I am learning Java");
+		System.out.print("Oh wow, learning Java is Fun. ");
+		System.out.print(" Hurray, I am happy");
+		System.out.printf(" Java was created in %d", 1995);
+		System.out.printf(" Java was made free for %.2f dollar",0.0000);
+		
+	}
+}
