628e5bfc4cdb162ee1a94c6ae44f00f9e7ec7322
beasticon36 <bozyboy360@gmail.com>
2026-05-07 14:24:42 +0100

    Keywords

---
+++ Keywords.java
@@ -0,0 +1,24 @@
+public class Keywords{
+	public static void main(String[] args){
+		System.out.println("abstract");
+		System.out.print("assert ");
+		System.out.print("\nboolean");
+		System.out.printf("\nbreak");
+		System.out.printf("\nbyte");
+		System.out.printf("\ncase");
+		System.out.printf("\ncatch");
+		System.out.printf("\nchar");
+		System.out.printf("\nclass");
+		System.out.printf("\nconst");
+		System.out.printf("\ncontinue");
+		System.out.printf("\ndefault");
+		System.out.printf("\ndo");
+		System.out.printf("\ndouble");
+		System.out.printf("\nelse");
+		System.out.printf("\nenum");
+		System.out.printf("\nexports");
+		System.out.printf("\nextends");
+		System.out.printf("\nfinal");
+		System.out.printf("\nfinally");
+	}
+}
